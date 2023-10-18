1287377d595a498e9c0d791eaeb70aaee8240327
unknown <vietnemo123@gmail.com>
2023-10-18 15:37:28 +1100

    small index.js fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-import App from './components/App';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import ToggleColorModeProvider from './utils/ToggleColorMode';
-import App from './components/App';
-import store from './app/store';
-import './index.css';
+import ToggleColorModeProvider from "./utils/ToggleColorMode";
+import App from "./components/App";
+import store from "./app/store";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -22,6 +19,5 @@ ReactDOM.render(
       </BrowserRouter>
     </ToggleColorModeProvider>
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
-
